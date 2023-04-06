d109254aa0c3c428b473ec117acb8aa7b9ec483c
kenanakperov <kenanakperov5gmail.com>
2023-04-07 00:46:27 +0400

    hello

---
+++ index.js
@@ -0,0 +1,110 @@
+// ! task 1 1kvM ə 2L kraska gedir 1 pəncərə 2kvM 1 qapı 4kvM dir 
+
+// width = parseInt(prompt("Eni daxil edin: ")) 
+// height = parseInt(prompt("Hündürlüyü daxil edin:"))
+// door = parseInt(prompt("Qapı sayını daxil edin:"))
+// pencere = parseInt(prompt("Pəncərə sayını daxil edin:"))
+
+
+
+
+// class calcPaint {
+//     constructor(width, height, door, pencere) {
+//         this.width = width;
+//         this.height = height;
+//         this.door = door;
+//         this.pencere = pencere;
+//     }
+//     amountPaint() {
+//         console.log(`
+//         Otağın ümumi sahəsi: ${this.width*this.height*4}kvM
+//         Otağın boyanmasına istifadə olunan boya miqdarı: ${this.width*this.height*8 - this.door*4- this.pencere*2}L`)
+//     }
+// }
+
+// const house = new calcPaint(width,height,door,pencere)
+
+// house.amountPaint()
+
+
+// ! task 2 aylar
+
+// let month  = parseInt(prompt("Ay daxil edin:"))
+
+// switch(month){
+//     case 1 :
+//         console.log("Yanvar");
+//         break;
+//     case 2 :
+//         console.log("Fevral");
+//         break;
+//     case 3 :
+//         console.log("Mart");
+//         break;
+//     case 4 :
+//         console.log("Aprel");
+//         break;
+//     case 5 :
+//         console.log("May");
+//         break;
+//     case 6 :
+//         console.log("Iyun");
+//         break;
+//     case 7 :
+//         console.log("Iyul");
+//         break;
+//     case 8 :
+//         console.log("Avqust");
+//         break;
+//     case 9 :
+//         console.log("Sentyabr");
+//         break;
+//     case 10 :
+//         console.log("Oktyabr");
+//         break;
+//     case 11 :
+//         console.log("Noyabr");
+//         break;
+//     case 12 :
+//         console.log("Dekabr");
+//         break;
+//         default: console.log("Belə ay yoxdur")
+// }
+
+
+
+
+// ! task 3 masin melumatlarini verme
+
+
+// let car = prompt("Avtomobili daxil edin:")
+
+// class carModels{
+//     constructor(model, color){
+//         this.model = model
+//         this.color = color
+//     }
+// }
+
+
+// const mercedes = new carModels("mercedes", "black");
+// const porcshe = new carModels("porcshe","black");
+// const bmw = new carModels("bmw", "red");
+// const toyota = new carModels("toyota", "blue")
+
+
+// switch(car){
+//     case "mercedes":
+//         console.log(mercedes);
+//         break
+//     case "porcshe":
+//         console.log(porcshe);
+//         break
+//     case "bmw":
+//         console.log(bmw);
+//         break
+//     case toyota:
+//         console.log(toyota)
+//         default:
+//             console.log("Bizdə belə avtomobil mövcud deyil!")
+// }
